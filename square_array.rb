835ea796b62add55cacd920c8aaf07579ff7a886
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-09 02:08:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  new_array = []          #to store and collect squared numbers
+  new_array = []              #to store and collect squared numbers
   array.each do |num|
-    new_array << num ** 2 #operate on each number of array & storing
+    new_array << num ** 2     #operate on each number of array & storing
   end
   new_array
 end
